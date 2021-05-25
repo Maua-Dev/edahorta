ac27806f4eacebb13c654c57ab09fe287732c5d6
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-05-25 20:03:09 -0300

    Primeiro commit agricultor

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -15,9 +15,6 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
-      ),
       body: Observer(
         builder: (context) => Text('${store.counter}'),
       ),

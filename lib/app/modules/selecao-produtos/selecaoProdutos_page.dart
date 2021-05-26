716316c5c0bfef101bfc9656cf26a19b0579b5d8
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2021-05-26 00:16:15 -0300

    ajustes de layout e cor

--- lib/app/modules/selecao-produtos/selecaoProdutos_page.dart
+++ lib/app/modules/selecao-produtos/selecaoProdutos_page.dart
@@ -16,68 +16,77 @@ class SelecaoProdutosPageState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarLogo(),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.separated(
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) => Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.all(16),
-                          leading: Container(
-                            height: 64,
-                            width: 64,
-                            child: Image.asset(
-                                controller.mercadorias[index].pathAsset),
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
                           ),
-                          title: Text(
-                            controller.mercadorias[index].name,
-                            style: TextStyle(fontSize: 24),
+                          child: ListTile(
+                            contentPadding: EdgeInsets.all(16),
+                            leading: Container(
+                              height: 64,
+                              width: 64,
+                              child: Image.asset(
+                                  controller.mercadorias[index].pathAsset),
+                            ),
+                            title: Text(
+                              controller.mercadorias[index].name,
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            trailing: Icon(Icons.arrow_forward_ios_outlined),
+                            onTap: () => controller.selectMercadoria(
+                                mercadoria: controller.mercadorias[index]),
                           ),
-                          trailing: Icon(Icons.arrow_forward_ios_outlined),
-                          onTap: () => controller.selectMercadoria(
-                              mercadoria: controller.mercadorias[index]),
                         ),
-                      ),
-                  separatorBuilder: (context, index) => SizedBox(
-                        height: 8,
-                      ),
-                  itemCount: controller.mercadorias.length),
-            )),
-            Material(
-              elevation: 8,
-              shape: RoundedRectangleBorder(
+                    separatorBuilder: (context, index) => SizedBox(
+                          height: 8,
+                        ),
+                    itemCount: controller.mercadorias.length),
+              ),
+              Positioned(
+                bottom: 0,
+                child: ClipRRect(
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(32),
                       topRight: Radius.circular(32)),
-                  side: BorderSide(color: Colors.grey)),
-              child: Container(
-                padding: EdgeInsets.all(8),
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: [
-                    Text(
-                      'Não Encontrou seu item? Clique aqui:',
-                      style: TextStyle(fontSize: 16),
+                  child: Material(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(32),
+                            topRight: Radius.circular(32)),
+                        side: BorderSide(color: Colors.grey)),
+                    elevation: 8,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        children: [
+                          Text(
+                            'Não Encontrou seu item? Clique aqui:',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          ElevatedButton.icon(
+                              onPressed: controller.selectMercadoria,
+                              icon: Icon(Icons.camera_alt),
+                              label: Text(
+                                'Adicionar manualmente',
+                                style: TextStyle(fontSize: 16),
+                              ))
+                        ],
+                      ),
                     ),
-                    ElevatedButton.icon(
-                        onPressed: controller.selectMercadoria,
-                        icon: Icon(Icons.camera_alt),
-                        label: Text(
-                          'Adicionar manualmente',
-                          style: TextStyle(fontSize: 16),
-                        ))
-                  ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );

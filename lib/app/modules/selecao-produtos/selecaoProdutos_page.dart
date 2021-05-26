630d8df2e4377397471a9aa0002cfb7ebbdaf58e
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2021-05-25 23:53:43 -0300

    add appbar

--- lib/app/modules/selecao-produtos/selecaoProdutos_page.dart
+++ lib/app/modules/selecao-produtos/selecaoProdutos_page.dart
@@ -1,3 +1,4 @@
+import 'package:edahorta/app/shared/widgets/appbar_logo_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:edahorta/app/modules/selecao-produtos/selecaoProdutos_controller.dart';
 import 'package:edahorta/app/enumeration/mercadorias_enum.dart';
@@ -14,6 +15,7 @@ class SelecaoProdutosPageState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBarLogo(),
       body: Container(
         child: Column(
           children: <Widget>[
